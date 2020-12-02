58f60c98dc4969a9dacf4e1bf384c86cfb5e2da9
Rachel <rjcrter11@gmail.com>
2020-12-02 07:49:05 -0600

    adds functionality for tabbed component

--- script.js
+++ script.js
@@ -3,14 +3,19 @@
 //////////////////////////////
 // ========== Selectors ========== //
 
+// Buttons 
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+
 // Navbar 
 const navLinks = document.querySelector('.nav__links');
 
 // Header 
 const header = document.querySelector('.header');
 
-// Buttons 
-const btnScrollTo = document.querySelector('.btn--scroll-to');
+// Operations 
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content')
 
 // Sections 
 const section1 = document.querySelector('#section--1');
@@ -80,9 +85,29 @@ btnScrollTo.addEventListener('click', () => {
 // Page Navigation
 
 navLinks.addEventListener('click', e => {
-    e.preventDefault()
+    e.preventDefault();
     if (e.target.classList.contains('nav__link')) {
         const id = e.target.getAttribute('href');
         document.querySelector(id).scrollIntoView({ behavior: 'smooth' })
     };
+});
+
+//////////////////////////////
+// Tabbed Component
+tabsContainer.addEventListener('click', (e) => {
+    const clicked = e.target.closest('.operations__tab');
+
+    // Guard clause 
+    if (!clicked) return;
+
+    // Remove active classes 
+    tabs.forEach(t => t.classList.remove('operations__tab--active'));
+    tabsContent.forEach(t => t.classList.remove('operations__content--active'));
+
+    // Activate tab
+    clicked.classList.add('operations__tab--active');
+
+    // Activate Content area
+    document.querySelector(`.operations__content--${clicked.dataset.tab}`).classList.add('operations__content--active');
+
 });
